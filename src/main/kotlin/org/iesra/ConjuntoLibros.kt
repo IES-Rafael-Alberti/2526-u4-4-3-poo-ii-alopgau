49f2444422fed4632d813f52c9535ee223b11198
alopgau <alopezg@ieslabahia.es>
2026-01-20 18:40:23 +0100

    Libro hecho

--- src/main/kotlin/org/iesra/ConjuntoLibros.kt
+++ src/main/kotlin/org/iesra/ConjuntoLibros.kt
@@ -1,5 +1,39 @@
 package org.iesra
 
-class ConjuntoLibros(val conjunto: Array<Libro>) {
+class ConjuntoLibros(val conjunto: Array<Libro?>) {
+    companion object {
+        const val limiteLibros = 5
+    }
+    var contador = 0
+    fun anadirLibro(libro: Libro): Boolean {
+        if (contador < limiteLibros && null in this.conjunto) {
+            conjunto[conjunto.indexOfFirst { it == null }] = libro
+            this.contador++
+            return true
+        }
+        return false
 
+    }
+    fun eliminarLibro(titulo: String, autor: String): Boolean {
+        val libroBuscado = conjunto.indexOfFirst {it?.titulo == titulo && it.autor == autor }
+        if (libroBuscado == -1) {
+            return false
+        } else {
+            conjunto[libroBuscado] = null
+            return true
+        }
+
+    }
+fun mostrarMejoryPeor() {
+    val peor = this.conjunto.filterNotNull().minByOrNull { it.nota }
+    val mejor =this.conjunto.filterNotNull().maxByOrNull { it.nota }
+    println(mejor)
+    println(peor)
 }
+    fun mostrarTodo(): Unit {
+        this.conjunto.filterNotNull().forEach { println(it) }
+    }
+}
+
+
+
--- src/main/kotlin/org/iesra/Libro.kt
+++ src/main/kotlin/org/iesra/Libro.kt
@@ -1,10 +1,10 @@
 package org.iesra
 
 class Libro(val titulo: String, val autor: String, val paginas: Int, var nota: Int) {
-companion object {
-    const val limiteLibros = 5
+    override fun toString(): String {
+        return "Título: ${this.titulo} Autor: ${this.autor} Páginas: ${paginas} Nota: ${nota}"
+    }
+    init {
+    require(nota > 0 && nota < 10)
 }
-
-
-
 }
