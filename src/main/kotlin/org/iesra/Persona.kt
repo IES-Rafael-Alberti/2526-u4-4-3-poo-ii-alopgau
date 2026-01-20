e790b4c48d37782055900d9a1d8bd6178f7ebbba
alopgau <alopezg@iesrafaelalberti.es>
2026-01-20 12:54:26 +0100

    Clase persona corregida

--- src/main/kotlin/org/iesra/Persona.kt
+++ src/main/kotlin/org/iesra/Persona.kt
@@ -1,11 +1,13 @@
 package org.iesra
 class Persona(var listaCuentas: Array<Cuenta?>, val dni: String) {
-    var cuentas = 0
-        private set
+    companion object {
+        const val limiteCuentas = 3
+    }
+    var contadorCuenta = 0
     fun anadirCuenta(cuenta: Cuenta): Boolean {
-        if (this.cuentas < 3) {
-            this.listaCuentas[cuentas] = cuenta
-            this.cuentas++
+        if (this.contadorCuenta < limiteCuentas) {
+            this.listaCuentas[contadorCuenta] = cuenta
+            this.contadorCuenta++
             return true
         }
         return false
